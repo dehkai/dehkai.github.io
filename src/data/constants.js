5a7209e677a7bb151298bdded3d0072b54bbcdd0
dehkai <ydk1421@gmail.com>
2024-03-20 17:06:36 +0800

    Update Resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/dehkai",
   resume:
-    "https://drive.google.com/file/d/1UMn7MAdo6J2JL4zJ9fsFgha-4yo6VxOd/view?usp=sharing",
+    "https://drive.google.com/file/d/1X8AlRbBGvFH40LfplxhAaQxjKxV2wH5l/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/yapdehkai/",
   twitter: "https://twitter.com/dehkai1",
   insta: "https://www.instagram.com/dk_0909_/",
