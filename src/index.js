9720ae1cdc22ea0667cf73e28ff8bf7ed563d174
dehkai <ydk1421@gmail.com>
2025-02-25 20:06:21 +0800

    Integrate Sentry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+
 import App from './App';
+
+Sentry.init({
+  dsn: "https://f0e1dd519b23a32ea0f4e04db3d4f79a@o4508879680897024.ingest.us.sentry.io/4508879688695808",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/dehkai\.me\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
